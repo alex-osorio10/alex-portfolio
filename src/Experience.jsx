b605e1575e1ff0e168b510d54bcbbbc8f7cfac02
Alexander Osorio <alexosorio1219@gmail.com>
2026-05-29 22:19:39 -0700

    updated experience again

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -18,7 +18,7 @@ function Experience() {
             </div>
 
             <div className="experience-entry">
-              <h3><strong>Walmart</strong> - <i>Retail Associate</i> (Feb. 2023 - June 2023)</h3>
+              <h3><strong>Walmart</strong> - <i>Retail Associate</i> (Feb. 2026 - Present)</h3>
               <p>▪ Elevated customer satisfaction and sales by delivering customer assistance and customer-friendly environment.</p>
               <p>▪ Arranged items on shelves and moved heavy crates of perishable food products to refrigerators.</p>
             </div>
@@ -33,12 +33,6 @@ function Experience() {
               <h3><strong>New Moon Aviation</strong> - <i>Outreach and Marketing Assistant</i> (Jul. 2025 - Dec. 2025)</h3>
               <p>▪ Assisted with website updates, flyers, social media, and event promotions.</p>
               <p>▪ Advanced outreach and visibility through marketing, contributing to rebranding efforts.</p>
-            </div>
-
-            <div className="experience-entry">
-              <h3><strong>Alticam Vision</strong> - <i>Intern</i> (June 2023 - Aug. 2023)</h3>
-              <p>▪ Enhanced Alticam’s customer user interface for consumer usability.</p>
-              <p>▪ Developed and optimized an extraction device for the user system, supporting entire subfolder extraction using C# and GitKraken.</p>
             </div>
           </div>
         </div>
