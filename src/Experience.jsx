b554688f9a65e826fa01859f0c4b5b10e99f41a5
Alexander Osorio <alexosorio1219@gmail.com>
2026-05-29 22:14:55 -0700

    Updated experience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -18,40 +18,27 @@ function Experience() {
             </div>
 
             <div className="experience-entry">
-              <h3><strong>Best Buy</strong> - <i>Seasonal Retail Sales Associate</i> (Oct. 2024 - Jan. 2025)</h3>
-              <p>▪ Drove customer engagement and sales by promoting benefits of memberships, contributing to store growth.</p>
-              <p>▪ Handled cashier duties, order pickups, and kept the sales floor clean and organized.</p>
-            </div>
-
-            <div className="experience-entry">
-              <h3><strong>IKEA</strong> - <i>Goods Flow Co-Worker</i> (Oct. 2021 - June 2024)</h3>
-              <p>▪ Improved teamwork efficiency, contributing to an improved customer service experience.</p>
-              <p>▪ Responsible for operating a delivery truck to facilitate seamless product deliveries.</p>
-              <p>▪ Transported heavy boxes for customer pickups and deliveries, ensuring efficient handling.</p>
-            </div>
-
-            <div className="experience-entry">
-              <h3><strong>FedEx Ground</strong> - <i>Package Handler</i> (Feb. 2023 - June 2023)</h3>
-              <p>▪ Optimized package handling processes, ensuring a seamless and organized workflow.</p>
-              <p>▪ Improved operational workflows by managing the transportation of heavy boxes for loading and unloading trucks.</p>
-            </div>
-
-            <div className="experience-entry">
-              <h3><strong>Walmart</strong> - <i>Food and Consumables</i> (Feb. 2021 - July 2021)</h3>
+              <h3><strong>Walmart</strong> - <i>Retail Associate</i> (Feb. 2023 - June 2023)</h3>
               <p>▪ Elevated customer satisfaction and sales by delivering customer assistance and customer-friendly environment.</p>
               <p>▪ Arranged items on shelves and moved heavy crates of perishable food products to refrigerators.</p>
             </div>
 
             <div className="experience-entry">
-              <h3><strong>Hood River Supply - ACE</strong> - <i>Service Station</i> (Nov. 2020 - Jan. 2021)</h3>
-              <p>▪ Maintained store efficiency by providing a positive and efficient customer experience.</p>
-              <p>▪ Pumped gas, stocked shelves, assisted customers, and sanitized restrooms.</p>
+              <h3><strong>Innovative Composite Engineering</strong> - <i>Composite Technician</i> (Jul. 2025 - Dec. 2025)</h3>
+              <p>▪ Improved efficiency and quality of materials by following precise methods.</p>
+              <p>▪ Operated tools and machinery to cut, laminate, wrap, cure, and finish parts.</p>
             </div>
 
             <div className="experience-entry">
-              <h3><strong>Safeway</strong> - <i>Courtesy Clerk</i> (Oct. 2019 - Oct. 2020)</h3>
-              <p>▪ Increased customer experience to increase sales growth.</p>
-              <p>▪ Bagged groceries, retrieved shopping carts, sanitized restrooms, and stocked shelves.</p>
+              <h3><strong>New Moon Aviation</strong> - <i>Outreach and Marketing Assistant</i> (Jul. 2025 - Dec. 2025)</h3>
+              <p>▪ Assisted with website updates, flyers, social media, and event promotions.</p>
+              <p>▪ Advanced outreach and visibility through marketing, contributing to rebranding efforts.</p>
+            </div>
+
+            <div className="experience-entry">
+              <h3><strong>Alticam Vision</strong> - <i>Intern</i> (June 2023 - Aug. 2023)</h3>
+              <p>▪ Enhanced Alticam’s customer user interface for consumer usability.</p>
+              <p>▪ Developed and optimized an extraction device for the user system, supporting entire subfolder extraction using C# and GitKraken.</p>
             </div>
           </div>
         </div>
